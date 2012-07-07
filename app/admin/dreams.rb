c831954ae5d34f4dd5d5c7717e78735bc9ccfa3b
Ryan Closner <ryan@ryanclosner.com>
2012-07-07 19:06:44 +0000

    Update admin#dream edit view to include portrait

--- app/admin/dreams.rb
+++ app/admin/dreams.rb
@@ -1,4 +1,7 @@
 ActiveAdmin.register Dream do
+
+  # Index Customizations
+
   index do
     column(:name)
     column(:location)
@@ -6,4 +9,25 @@ ActiveAdmin.register Dream do
     column(:created_at)
     default_actions
   end
+
+  filter(:name)
+  filter(:location)
+  filter(:age)
+  filter(:visible)
+  filter(:created_at)
+  filter(:updated_at)
+
+  # Form Customizations
+
+  form :html => { :enctype => "multipart/form-data" } do |f|
+    f.inputs do
+      f.input(:name)
+      f.input(:location)
+      f.input(:age, :as => :number)
+      f.input(:visible, :as => :radio)
+      f.input(:portrait, :as => :file, :hint => f.template.image_tag(f.object.portrait.url(:medium)))
+    end
+    f.buttons
+  end
+
 end
