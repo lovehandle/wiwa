3a540d9e3ce9e8050cdb97851978ec429931b1e9
Ryan Closner <ryan@ryanclosner.com>
2012-07-07 18:47:35 +0000

    Show only visible dreams on the index

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -1,2 +1,5 @@
 class DreamsController < InheritedResources::Base
+  def index
+    @dreams = Dream.visible
+  end
 end
--- app/models/dream.rb
+++ app/models/dream.rb
@@ -1,3 +1,5 @@
 class Dream < ActiveRecord::Base
   attr_accessible :age, :description, :location, :name, :visible
+
+  scope :visible, where(:visible => true)
 end
