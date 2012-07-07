40e607dc952f61bfbfe39c203fd6a3f6a5283ec9
Ryan Closner <ryan@ryanclosner.com>
2012-07-07 18:10:08 +0000

    Add RSpec to application

--- Gemfile
+++ Gemfile
@@ -12,4 +12,5 @@ end
 
 group :test do
   gem 'factory_girl_rails', '~> 3.5.0'
+  gem 'rspec-rails',        '~> 2.10.1'
 end
--- config/application.rb
+++ config/application.rb
@@ -62,6 +62,7 @@ module Wiwa
     # Generators
     config.generators do |g|
       g.fixture_replacement :factory_girl
+      g.test_framework      :rspec
     end
   end
 end
