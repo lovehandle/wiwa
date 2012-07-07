5ac952a51ad7b42edf21dbff4d6e0c9ea8842a0d
Ryan Closner <ryan@ryanclosner.com>
2012-07-07 20:40:07 +0000

    Modify DreamSubmission behavior

--- app/models/dream_submission.rb
+++ app/models/dream_submission.rb
@@ -1,3 +1,36 @@
 class DreamSubmission < ActiveRecord::Base
+
+  validates :age, :city, :country, :dream, :email, :first_name, :last_name, :state, :presence => true
+  validates :state,   :inclusion => { :in => Carmen.state_codes, :if => lambda { domestic? } }
+  validates :country, :inclusion => { :in => Carmen.country_names }
+
   attr_accessible :age, :city, :country, :dream, :email, :first_name, :last_name, :state
+
+  def domestic?
+    country == "United States"
+  end
+
+  def international?
+    !domestic?
+  end
+
+  def fullname
+    [first_name, last_name].compact.join(" ")
+  end
+
+  def location
+    ([city]).tap do |location|
+      location << state if domestic?
+      location << country if international?
+    end.compact.join(" ")
+  end
+
+  def attributes
+    { :name => fullname, :location => location, :description => dream }
+  end
+
+  def to_dream
+    Dream.new(attributes)
+  end
+
 end
--- spec/models/dream_submission_spec.rb
+++ spec/models/dream_submission_spec.rb
@@ -1,5 +1,67 @@
 require 'spec_helper'
 
 describe DreamSubmission do
-  pending "add some examples to (or delete) #{__FILE__}"
+  [ :state, :country, :first_name, :last_name, :email, :dream ].each do |attribute|
+    it { should validate_presence_of(attribute) }
+  end
+
+  context "submission is domestic" do
+    before do
+      subject.stub(:domestic?).and_return(true)
+    end
+
+    Carmen.state_codes.each do |state|
+      it { should allow_value(state).for(:state) }
+    end
+
+    it { should_not allow_value("XX").for(:state) }
+  end
+
+  context "submission is international" do
+    before do
+      subject.stub(:domestic?).and_return(false)
+    end
+
+    it { should allow_value("XX").for(:state) }
+  end
+
+  Carmen.country_names.each do |country|
+    it { should allow_value(country).for(:country) }
+  end
+
+  context "country = United States" do
+    before do
+      subject.country = "United States"
+    end
+
+    its(:domestic?)      { should be_true }
+    its(:international?) { should be_false }
+  end
+
+  context "country != United States" do
+    before do
+      subject.country = "Germany"
+    end
+
+    its(:domestic?)      { should be_false }
+    its(:international?) { should be_true }
+  end
+
+  describe "#location" do
+    let(:country) { "United States" }
+    let(:city)    { "Portland" }
+    let(:state)   { "OR" }
+
+    subject do
+      described_class.new(:country => country, :city => city, :state => state)
+    end
+    context "submission is domestic" do
+      its(:location) { should eql("Portland OR") }
+    end
+
+    context "submission is international" do
+      let(:country) { "Krakow"}
+      its(:location) { should eql("Portland Krakow")}
+    end
+  end
 end
