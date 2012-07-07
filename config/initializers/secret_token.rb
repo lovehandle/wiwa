16fd4de60f27a58ee10d7f4eb1b7e0159b471ee8
Ryan Closner <ryan@ryanclosner.com>
2012-07-07 18:04:31 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wiwa::Application.config.secret_token = 'b88f886e44476f6e47e5315c9353bfd3623904fa7c7654f2412148f7eab0fd060878513e3942adbdd7644ff7a1771252ce2eeb6ca3682898a49a01c40d6b7d0a'
